bdbfbeb08d6316c355abd739e4acc30f4cc2f919
Vincent Fung <vfung26@my.bcit.ca>
2024-03-19 14:15:28 -0700

    made the navbar its own html file, and loads in with js

---
+++ scripts/skeleton.js
@@ -0,0 +1,18 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton() {
+
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {                   //if the pointer to "user" object is not null, then someone is logged in
+            // User is signed in.
+            // Do something for the user here.
+            console.log($('#footerPlaceholder').load('./navbar/nav_after_login/.html'));
+        } else {
+            // No user is signed in.
+            console.log($('#footerPlaceholder').load('./navbar/nav_before_login.html'));
+        }
+    });
+}
+loadSkeleton(); //invoke the function
