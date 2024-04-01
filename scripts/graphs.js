69328a998b158b6ca7edb9909c5e12c7794ba403
Vincent Fung <vfung26@my.bcit.ca>
2024-03-31 20:46:51 -0700

    added graphs api

--- scripts/graphs.js
+++ scripts/graphs.js
@@ -1,30 +1,109 @@
-function displayGraphMockData(collection) {
-    db.collection(collection).get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                document.getElementById("mockGraphs").innerHTML =
-                `
-                January: ${doc.data().jan}<br/>
-                February: ${doc.data().feb}<br/>
-                March: ${doc.data().mar}<br/>
-                April: ${doc.data().apr}<br/>
-                May: ${doc.data().may}<br/>
-                June: ${doc.data().june}<br/>
-                July: ${doc.data().july}<br/>
-                August: ${doc.data().aug}<br/>
-                September: ${doc.data().sep}<br/>
-                October: ${doc.data().oct}<br/>
-                November: ${doc.data().nov}<br/>
-                December: ${doc.data().dec}
-                `;
-            });
-        })
-        .catch((error) => {
-            console.error("Error getting documents: ", error);
-        });
-}
-displayGraphMockData("graphs");
+// function displayGraphMockData(collection) {
+//     db.collection(collection).get()
+//         .then((querySnapshot) => {
+//             querySnapshot.forEach((doc) => {
+//                 document.getElementById("mockGraphs").innerHTML =
+//                 `
+//                 January: ${doc.data().jan}<br/>
+//                 February: ${doc.data().feb}<br/>
+//                 March: ${doc.data().mar}<br/>
+//                 April: ${doc.data().apr}<br/>
+//                 May: ${doc.data().may}<br/>
+//                 June: ${doc.data().june}<br/>
+//                 July: ${doc.data().july}<br/>
+//                 August: ${doc.data().aug}<br/>
+//                 September: ${doc.data().sep}<br/>
+//                 October: ${doc.data().oct}<br/>
+//                 November: ${doc.data().nov}<br/>
+//                 December: ${doc.data().dec}
+//                 `;
+//             });
+//         })
+//         .catch((error) => {
+//             console.error("Error getting documents: ", error);
+//         });
+// }
+// displayGraphMockData("graphs");
 
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    },
+    {
+        value: 40,
+        color: "#949FB1",
+        highlight: "#A8B3C5",
+        label: "Grey"
+    },
+    {
+        value: 120,
+        color: "#4D5360",
+        highlight: "#616774",
+        label: "Dark Grey"
+    }
+
+];
+
+var data2 = [
+    {
+        value: 75,
+        color:"purple",
+        highlight: "#FF5A5E",
+        label: "Purple"
+    },
+    {
+        value: 12,
+        color: "green",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 8,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    },
+    {
+        value: 95,
+        color: "#949FB1",
+        highlight: "#A8B3C5",
+        label: "Grey"
+    },
+    {
+        value: 120,
+        color: "#4D5360",
+        highlight: "#616774",
+        label: "Dark Grey"
+    }
+
+];
+
+var ctx = document.getElementById("myChart").getContext("2d");
+
+function one() {
+    var myNewChart = new Chart(ctx).PolarArea(data);
+}
+
+function two() {
+    var myNewChart = new Chart(ctx).PolarArea(data2);
+}
+
+one();
 
 // function writeGraphData() {
 //     var graphRef = db.collection("graphs");
--- scripts/themes.js
+++ scripts/themes.js
@@ -4,27 +4,27 @@ function displayThemeMockData(collection) {
             querySnapshot.forEach((doc) => {
                 document.getElementById("themeDrop").innerHTML =
                 `
-                Hat 1: ${doc.data().hat1}<br/>
-                Hat 2: ${doc.data().hat2}<br/>
-                Hat 3: ${doc.data().hat3}<br/>
-                Hat 4: ${doc.data().hat4}<br/>
-                Hat 5: ${doc.data().hat5}<br/>
+                hat 1: ${doc.data().hat1}<br/>
+                hat 2: ${doc.data().hat2}<br/>
+                hat 3: ${doc.data().hat3}<br/>
+                hat 4: ${doc.data().hat4}<br/>
+                hat 5: ${doc.data().hat5}<br/>
                 hat 6: ${doc.data().hat6}<br/>
                 hat 7: ${doc.data().hat7}<br/>
                 hat 8: ${doc.data().hat8}<br/>
-                Hat 9: ${doc.data().hat9}<br/>
-                Hat 10: ${doc.data().hat10}<br/>
-                Hat 11: ${doc.data().hat11}<br/>
-                Hat 12: ${doc.data().hat12}<br/>
-                Hat 13: ${doc.data().hat13}<br/>
+                hat 9: ${doc.data().hat9}<br/>
+                hat 10: ${doc.data().hat10}<br/>
+                hat 11: ${doc.data().hat11}<br/>
+                hat 12: ${doc.data().hat12}<br/>
+                hat 13: ${doc.data().hat13}<br/>
                 hat 14: ${doc.data().hat14}<br/>
                 hat 15: ${doc.data().hat15}<br/>
                 hat 16: ${doc.data().hat16}<br/>
-                Hat 17: ${doc.data().hat17}<br/>
-                Hat 18: ${doc.data().hat18}<br/>
-                Hat 19: ${doc.data().hat19}<br/>
-                Hat 20: ${doc.data().hat20}<br/>
-                Hat 21: ${doc.data().hat21}
+                hat 17: ${doc.data().hat17}<br/>
+                hat 18: ${doc.data().hat18}<br/>
+                hat 19: ${doc.data().hat19}<br/>
+                hat 20: ${doc.data().hat20}<br/>
+                hat 21: ${doc.data().hat21}
                 `;
             });
         })
@@ -38,27 +38,27 @@ displayThemeMockData("themes");
 // function writeThemesData() {
 //     var themeRef = db.collection("themes");
 //     themeRef.add({
-//         Hat1: "1",
-//         Hat2: "4",
-//         Hat3: "57",
-//         Hat4: "34",
-//         Hat5: "25",
-//         Hat6: "26",
-//         Hat7: "85",
-//         Hat8: "31",
-//         Hat9: "16",
-//         Hat10:"52",
-//         Hat11:"62",
-//         Hat12:"74",
-//         Hat13:"53",
-//         Hat14:"39",
-//         Hat15:"27",
-//         Hat16:"19",
-//         Hat17:"69",
-//         Hat18:"42",
-//         Hat19:"21",
-//         Hat20:"70",
-//         Hat21:"40"
+//         hat1: "1",
+//         hat2: "4",
+//         hat3: "57",
+//         hat4: "34",
+//         hat5: "25",
+//         hat6: "26",
+//         hat7: "85",
+//         hat8: "31",
+//         hat9: "16",
+//         hat10:"52",
+//         hat11:"62",
+//         hat12:"74",
+//         hat13:"53",
+//         hat14:"39",
+//         hat15:"27",
+//         hat16:"19",
+//         hat17:"69",
+//         hat18:"42",
+//         hat19:"21",
+//         hat20:"70",
+//         hat21:"40"
 //     });
 //     console.log("theme mock data has been added.");
 // }
