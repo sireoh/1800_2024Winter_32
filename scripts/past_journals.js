d4f03f77d95a9d2a485afa6ced7aa42d6d765eab
MansibTR <nabil99002@gmail.com>
2024-04-10 22:38:21 -0700

    1) Added a delete icon in journal entries to delete past entries. 2) Fixed some button and font sizes.

--- scripts/past_journals.js
+++ scripts/past_journals.js
@@ -13,6 +13,44 @@ function formatTimestamp(timestamp) {
     return date.toLocaleString('en-US', options);
 }
 
+// Function to delete a journal entry from Firestore
+function deleteJournalEntry(entryId) {
+    var modal = document.getElementById("deleteModal");
+    var confirmBtn = document.getElementById("confirmDeleteBtn");
+    var cancelBtn = document.getElementById("cancelDeleteBtn");
+
+    // Show modal dialog
+    modal.style.display = "block";
+
+    // Close modal when the close button is clicked
+    var closeButton = document.getElementsByClassName("close")[0];
+    closeButton.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    // Hide modal when cancel button is clicked
+    cancelBtn.onclick = function() {
+        modal.style.display = "none";
+    };
+
+    // Delete entry when confirm button is clicked
+    confirmBtn.onclick = function() {
+        db.collection("journals").doc(entryId).delete()
+            .then(function() {
+                console.log("Document successfully deleted!");
+                // Remove the deleted entry from the UI
+                var entryToRemove = document.getElementById(entryId);
+                if (entryToRemove) {
+                    entryToRemove.remove();
+                }
+                modal.style.display = "none"; // Hide modal after deletion
+            })
+            .catch(function(error) {
+                console.error("Error removing document: ", error);
+            });
+    };
+}
+
 // Function to load past journals of the logged-in user
 function loadPastJournals(user) {
     if (user) {
@@ -29,9 +67,11 @@ function loadPastJournals(user) {
                     var journalDiv = document.createElement("div");
                     journalDiv.classList.add("journal-entry");
                     journalDiv.dataset.timestamp = journalData.timestamp.toDate().toString(); // Add timestamp as data attribute
+                    journalDiv.id = doc.id; // Set the ID of the div to the document ID
                     var formattedTimestamp = formatTimestamp(journalData.timestamp);
-                    journalDiv.innerHTML =  "<p id=\"time\" +>" + formattedTimestamp + "</p>" + 
-                                "<br>" + "<p>" + journalData.description + "</p>";
+                    journalDiv.innerHTML =  "<p id=\"time\">" + formattedTimestamp + "</p>" + 
+                                "<br>" + "<p>" + journalData.description + "</p>" +
+                                "<br><i class=\"fas fa-trash-alt delete-icon\" onclick=\"deleteJournalEntry('" + doc.id + "')\"></i>"; // Add delete icon with onclick event
                     // Append the div to the journalContainer div
                     document.getElementById("journalContainer").appendChild(journalDiv);
                 });
